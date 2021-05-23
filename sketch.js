4c9e0b291d8c0a13f688ed873a9ffc1ad8a2acf1
Ruta-123 <72984772+Ruta-123@users.noreply.github.com>
2021-05-23 15:51:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,88 @@
+var database ,dog,dog1,dog2
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
 //Create variables here
-var dog, happyDog, database, foodS, foodStock;
-var dogImage, happyDogImage;
-
 
 function preload()
+
 {
-	dogImage = loadImage("Images/Dog.png")
-  happyDogImage = loadImage("Images/happydog.png")
+  dogimg1 = loadImage("images/dogImg.png")
+  dogimg2 = loadImage("images/dogImg1.png")
+	//load images here
 }
 
 function setup() {
-  database = firebase.database()
-	createCanvas(500, 500);
-  dog = createSprite(250, 250)
-  dog.addImage(dogImage)
-  dog.scale = 0.2;
-  foodStock = database.ref('Food')
-  foodStock.on("value", readStock)
+	createCanvas(1000, 500);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+ 
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+  feed = createButton("FEED DRAGO")
+  feed.position(500,15)
+  feed.mousePressed(FeedDog)
+  add = createButton("ADD FOOD")
+  add.position(400,15)
+  add.mousePressed(AddFood)
+
+} 
+
+function draw(){
+ background(46,139,87);
+
+ foodobject.display()
+ 
+ drawSprites();
+  
+ fill(255,255,254);
+ textSize(15);
+
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
 }
 
-
-function draw() {  
-  background(46, 139, 87)
-
-  //add styles here
-  if(keyWentDown(UP_ARROW)){
-    writeStock(foodS)
-    dog.addImage(happyDogImage)
-  }
-  drawSprites();
-  fill("red")
-  textSize(25)
-  stroke("black")
-  strokeWeight(2)
-  text("Note : Press UP Aroow to feed the dog", 20, 30)
-
+function showError(){
+  console.log("Error in writing to the database");
 }
 
-function readStock(data)
-{
-  foodS = data.val()
-}
-
-function writeStock(x)
-{
-  if(x >= 0){
-    x = 0
+function writePosition(nazo){
+  if(nazo>0){
+    nazo=nazo-1
   }
   else{
-    x-=1
+    nazo=0
   }
-  database.ref('/').update(
-    {
-      Food: x
-    }
-  )
+  database.ref('/').set({
+    'Food': nazo
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function FeedDog(){
+
+dog.addImage(dogimg2)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
 }
